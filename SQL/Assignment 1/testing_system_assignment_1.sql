f4d8b6ed92bed79b5c055e4e87fa2552c8100938
baronnmc <chuongnguyen.dc@gmail.com>
2021-10-21 22:32:21 +0700

    Update Assigntment 1&2

--- SQL/Assignment 1/testing_system_assignment_1.sql
+++ SQL/Assignment 1/testing_system_assignment_1.sql
@@ -1,29 +1,30 @@
+DROP DATABASE testing_system_assignment_1;
 CREATE DATABASE testing_system_assignment_1;
 USE testing_system_assignment_1;
 
 CREATE TABLE department(
 	department_id	TINYINT,
-    department_name	CHAR(30)
+    department_name	VARCHAR(30)
 );
     
 CREATE TABLE `position`(
 	position_id		TINYINT,
-    position_name	CHAR(30)
+    position_name	VARCHAR(30)
 );
 
 CREATE TABLE `account`(
 	account_id		TINYINT,
-    email			CHAR(30),
-    username		CHAR(30),
-    fullname		CHAR(50),
+    email			VARCHAR(30),
+    username		VARCHAR(30),
+    fullname		VARCHAR(50),
     department_id	TINYINT,
     position_id		TINYINT,
     create_date		DATE
 );
 
-CREATE TABLE group1(
+CREATE TABLE `group`(
 	group_id		TINYINT,
-    group_name		CHAR(30),
+    group_name		VARCHAR(30),
     creator_id		TINYINT,
     create_date		DATE
 );
@@ -33,3 +34,15 @@ CREATE TABLE group_acccount(
     account_id		TINYINT,
     join_date		DATE
 );
+
+INSERT INTO department(department_id,department_name)
+VALUES	(1,	'Marketing'	),
+		(2,	'Sale'			),
+        (3,	'Bảo vệ'		),
+        (4,	'Nhân sự'		),
+        (5,	'Kỹ thuật'		),
+        (6,	'Tài chính'		),
+        (7,	'Phó giám đốc'	),
+        (8,	'Giám đốc'		),
+        (9,	'Thư ký'		),
+        (10,'Bán hàng'		);
